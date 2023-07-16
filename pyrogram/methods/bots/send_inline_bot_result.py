2ccba3cc4f03895e12c811f8afa694f85e3b43ab
bootysheer888 <139667656+bootysheer888@users.noreply.github.com>
2023-07-16 05:25:09 +0100

    Update send_inline_bot_result.py

--- pyrogram/methods/bots/send_inline_bot_result.py
+++ pyrogram/methods/bots/send_inline_bot_result.py
@@ -20,28 +20,22 @@ from typing import Union
 
 import pyrogram
 from pyrogram import raw
-from pyrogram import types
 
 
-class SendGame:
-    async def send_game(
+class SendInlineBotResult:
+    async def send_inline_bot_result(
         self: "pyrogram.Client",
         chat_id: Union[int, str],
-        game_short_name: str,
+        query_id: int,
+        result_id: str,
         disable_notification: bool = None,
         message_thread_id: int = None,
-        reply_to_message_id: int = None,
-        protect_content: bool = None,
-        reply_markup: Union[
-            "types.InlineKeyboardMarkup",
-            "types.ReplyKeyboardMarkup",
-            "types.ReplyKeyboardRemove",
-            "types.ForceReply"
-        ] = None
-    ) -> "types.Message":
-        """Send a game.
+        reply_to_message_id: int = None
+    ):
+        """Send an inline bot result.
+        Bot results can be retrieved using :meth:`~pyrogram.Client.get_inline_bot_results`
 
-        .. include:: /_includes/usable-by/bots.rst
+        .. include:: /_includes/usable-by/users.rst
 
         Parameters:
             chat_id (``int`` | ``str``):
@@ -49,57 +43,34 @@ class SendGame:
                 For your personal cloud (Saved Messages) you can simply use "me" or "self".
                 For a contact that exists in your Telegram address book you can use his phone number (str).
 
-            game_short_name (``str``):
-                Short name of the game, serves as the unique identifier for the game. Set up your games via Botfather.
+            query_id (``int``):
+                Unique identifier for the answered query.
+
+            result_id (``str``):
+                Unique identifier for the result that was chosen.
 
             disable_notification (``bool``, *optional*):
                 Sends the message silently.
                 Users will receive a notification with no sound.
 
-            message_thread_id (``int``, *optional*):
-                Unique identifier of a message thread to which the message belongs.
-                for supergroups only
-
-            reply_to_message_id (``int``, *optional*):
+            reply_to_message_id (``bool``, *optional*):
                 If the message is a reply, ID of the original message.
 
-            protect_content (``bool``, *optional*):
-                Protects the contents of the sent message from forwarding and saving.
-
-            reply_markup (:obj:`~pyrogram.types.InlineKeyboardMarkup`, *optional*):
-                An object for an inline keyboard. If empty, one ‘Play game_title’ button will be shown automatically.
-                If not empty, the first button must launch the game.
-
         Returns:
-            :obj:`~pyrogram.types.Message`: On success, the sent game message is returned.
+            :obj:`~pyrogram.types.Message`: On success, the sent inline result message is returned.
 
         Example:
             .. code-block:: python
 
-                await app.send_game(chat_id, "gamename")
+                await app.send_inline_bot_result(chat_id, query_id, result_id)
         """
-        r = await self.invoke(
-            raw.functions.messages.SendMedia(
+        return await self.invoke(
+            raw.functions.messages.SendInlineBotResult(
                 peer=await self.resolve_peer(chat_id),
-                media=raw.types.InputMediaGame(
-                    id=raw.types.InputGameShortName(
-                        bot_id=raw.types.InputUserSelf(),
-                        short_name=game_short_name
-                    ),
-                ),
-                message="",
-                silent=disable_notification or None,
-                reply_to_msg_id=reply_to_message_id or message_thread_id,
+                query_id=query_id,
+                id=result_id,
                 random_id=self.rnd_id(),
-                noforwards=protect_content,
-                reply_markup=await reply_markup.write(self) if reply_markup else None
+                silent=disable_notification or None,
+                reply_to_msg_id=reply_to_message_id or message_thread_id
             )
         )
-
-        for i in r.updates:
-            if isinstance(i, (raw.types.UpdateNewMessage, raw.types.UpdateNewChannelMessage)):
-                return await types.Message._parse(
-                    self, i.message,
-                    {i.id: i for i in r.users},
-                    {i.id: i for i in r.chats}
-                )
